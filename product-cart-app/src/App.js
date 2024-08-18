97d46030da1e81bb8206d5736d7a6d2cc1883842
Ananthbarath <ananthbarath2001@gmail.com>
2024-08-18 13:59:08 +0530

    final

--- product-cart-app/src/App.js
+++ product-cart-app/src/App.js
@@ -4,15 +4,14 @@ import ProductList from './components/ProductList';
 import CartPage from './components/CartPage';
 import { CartProvider } from './context/CartContext';
 import "./App.css"
-import SignupPage from './components/SignupPage';
 
 function App() {
   return (
     <CartProvider>
       <Router>
         <Routes>
-          <Route path='/signup' element={<SignupPage/>}/>
-          <Route path="/product" element={<ProductList />} />
+          
+          <Route path="/" element={<ProductList />} />
           <Route path="/cart" element={<CartPage />} />
         </Routes>
       </Router>
